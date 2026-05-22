1f9d49b236fd19bc7c7626619285a6980c11d4b4
shubha-2018 <thoratshubha2018@gmail.com>
2026-05-22 11:32:27 +0530

    sp

--- src/components/KokanRange.tsx
+++ src/components/KokanRange.tsx
@@ -9,19 +9,19 @@ const KokanRangeTable: React.FC = () => {
   const { language } = useLanguageContext();
 
   const data = [
-    { sr: 1, commissionerate: { marathi: "ठाणे शहर", english: "Thane City" }, designation: { marathi: "सहा. पोलीस आयुक्त", english: "Police Inspector" }, officerName: { marathi: "नासिर पठाण", english: "Nasir Pathan" }, contact: "9664857867" },
-    { sr: 2, commissionerate: { marathi: "पोलीस नौका कर्मशाळा, ठाणे", english: "Police Boat Workshop, Thane" }, designation: { marathi: "पोलीस उप-अधीक्षक", english: "Deputy Superintendent of Police" }, officerName: { marathi: "-", english: "-" }, contact: "-" },
-    { sr: 3, commissionerate: { marathi: "नवी मुंबई शहर", english: "Navi Mumbai City" }, designation: { marathi: "पोलीस निरीक्षक", english: "Police Inspector" }, officerName: { marathi: "सचिन दंताल", english: "Sachin Dantal" }, contact: "8108383006" },
+    { sr: 1, commissionerate: { marathi: "ठाणे शहर", english: " M.T. Thane City" }, designation: { marathi: "सहा. पोलीस आयुक्त", english: "Police Inspector" }, officerName: { marathi: "नासिर पठाण", english: "Nasir Pathan" }, contact: "9664857867" },
+    { sr: 2, commissionerate: { marathi: "पोलीस नौका कर्मशाळा, ठाणे", english: " M.T. Police Boat Workshop, Thane" }, designation: { marathi: "पोलीस उप-अधीक्षक", english: "Deputy Superintendent of Police" }, officerName: { marathi: "-", english: "-" }, contact: "-" },
+    { sr: 3, commissionerate: { marathi: "नवी मुंबई शहर", english: " M.T. Navi Mumbai City" }, designation: { marathi: "पोलीस निरीक्षक", english: "Police Inspector" }, officerName: { marathi: "सचिन दंताल", english: "Sachin Dantal" }, contact: "8108383006" },
     // { sr: 4, commissionerate: { marathi: "-", english: "-" }, designation: { marathi: "पोलीस निरीक्षक", english: "Police Inspector" }, officerName: { marathi: "प्रभाकर झेंडे", english: "Prabhakar Zende" }, contact: "9823112469" },
-    { sr: 4, commissionerate: { marathi: "नाशिक शहर", english: "Nashik City" }, designation: { marathi: "पोलीस निरीक्षक", english: "Police Inspector" }, officerName: { marathi: "पंकज पवार", english: "Pankaj Pawar" }, contact: "9923430414" },
-    { sr: 5, commissionerate: { marathi: "ठाणे ग्रामीण", english: "Thane Rural" }, designation: { marathi: "पोलीस निरीक्षक", english: "Police Inspector" }, officerName: { marathi: "बाळासाहेब दत्तात्रय झेंडे", english: "Balasaheb Dattatray Zende" }, contact: "9823112469" },
-    { sr: 6, commissionerate: { marathi: "पालघर", english: "Palghar" }, designation: { marathi: "-", english: "-" }, officerName: { marathi: "-", english: "-" }, contact: "-" },
-    { sr: 7, commissionerate: { marathi: "नाशिक ग्रामीण", english: "Nashik Rural" }, designation: { marathi: "पोलीस निरीक्षक", english: "Police Inspector" }, officerName: { marathi: "अनिल घाडगे", english: "Anil Ghadage" }, contact: "9823168900" },
-    { sr: 8, commissionerate: { marathi: "रायगड", english: "Raigad" }, designation: { marathi: "पोलीस उपनिरीक्षक", english: "Police Sub Inspector" }, officerName: { marathi: "मुत्सुफा शेख", english: "Mustufa Shaikh" }, contact: "9881359199" },
-    { sr: 9, commissionerate: { marathi: "रत्नागिरी", english: "Ratnagiri" }, designation: { marathi: "-", english: "-" }, officerName: { marathi: "-", english: "-" }, contact: "-" },
-    { sr: 10, commissionerate: { marathi: "सिंधुदुर्ग", english: "Sindhudurg" }, designation: { marathi: "पोलीस उपनिरीक्षक", english: "Police Sub Inspector" }, officerName: { marathi: "प्रदीप चव्हाण", english: "Pradip Chavan" }, contact: "8108343636" },
-    { sr: 11, commissionerate: { marathi: "एसआरपीएफ ग्रुप 8, मुंबई", english: "SRPF Group 8, Mumbai" }, designation: { marathi: "पोलीस निरीक्षक", english: "Police Inspector" }, officerName: { marathi: "अशोक निमबाळकर", english: "Ashok Nimbalkar" }, contact: "9372928211" },
-    { sr: 12, commissionerate: { marathi: "एसआरपीएफ ग्रुप 11, नवी मुंबई", english: "SRPF Group 11, Navi Mumbai" }, designation: { marathi: "पोलीस निरीक्षक", english: "Police Inspector" }, officerName: { marathi: "विठ्ठल बडे", english: "Vitthal Badhe" }, contact: "9773071673" },
+    { sr: 4, commissionerate: { marathi: "नाशिक शहर", english: "M.T. Nashik City" }, designation: { marathi: "पोलीस निरीक्षक", english: "Police Inspector" }, officerName: { marathi: "पंकज पवार", english: "Pankaj Pawar" }, contact: "9923430414" },
+    { sr: 5, commissionerate: { marathi: "ठाणे ग्रामीण", english: "M.T. Thane Rural" }, designation: { marathi: "पोलीस निरीक्षक", english: "Police Inspector" }, officerName: { marathi: "बाळासाहेब दत्तात्रय झेंडे", english: "Balasaheb Dattatray Zende" }, contact: "9823112469" },
+    { sr: 6, commissionerate: { marathi: "पालघर", english: "M.T. Palghar" }, designation: { marathi: "-", english: "-" }, officerName: { marathi: "-", english: "-" }, contact: "-" },
+    { sr: 7, commissionerate: { marathi: "नाशिक ग्रामीण", english: "M.T. Nashik Rural" }, designation: { marathi: "पोलीस निरीक्षक", english: "Police Inspector" }, officerName: { marathi: "अनिल घाडगे", english: "Anil Ghadage" }, contact: "9823168900" },
+    { sr: 8, commissionerate: { marathi: "रायगड", english: "M.T. Raigad" }, designation: { marathi: "पोलीस उपनिरीक्षक", english: "Police Sub Inspector" }, officerName: { marathi: "मुत्सुफा शेख", english: "Mustufa Shaikh" }, contact: "9881359199" },
+    { sr: 9, commissionerate: { marathi: "रत्नागिरी", english: "M.T.Ratnagiri" }, designation: { marathi: "-", english: "-" }, officerName: { marathi: "-", english: "-" }, contact: "-" },
+    { sr: 10, commissionerate: { marathi: "सिंधुदुर्ग", english: "M.T.Sindhudurg" }, designation: { marathi: "पोलीस उपनिरीक्षक", english: "Police Sub Inspector" }, officerName: { marathi: "प्रदीप चव्हाण", english: "Pradip Chavan" }, contact: "8108343636" },
+    { sr: 11, commissionerate: { marathi: "एसआरपीएफ ग्रुप 8, मुंबई", english: "M.T. SRPF Group 8, Mumbai" }, designation: { marathi: "पोलीस निरीक्षक", english: "Police Inspector" }, officerName: { marathi: "अशोक निमबाळकर", english: "Ashok Nimbalkar" }, contact: "9372928211" },
+    { sr: 12, commissionerate: { marathi: "एसआरपीएफ ग्रुप 11, नवी मुंबई", english: "M.T. SRPF Group 11, Navi Mumbai" }, designation: { marathi: "पोलीस निरीक्षक", english: "Police Inspector" }, officerName: { marathi: "विठ्ठल बडे", english: "Vitthal Badhe" }, contact: "9773071673" },
   ];
 
   return (
